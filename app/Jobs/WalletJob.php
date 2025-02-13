be39485ea78fedf30ddc1d7e24545045edec7f1f
Jack <nguijack@gmail.com>
2025-02-13 23:28:42 +0800

    create rebate job

---
+++ app/Jobs/WalletJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Transaction;
+use App\Models\Wallet;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class WalletJob implements ShouldQueue
+{
+    use Queueable;
+
+    protected $walletId;
+    protected $amount;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($walletId, $amount)
+    {
+        $this->walletId = $walletId;
+        $this->amount = $amount;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $wallet = Wallet::find($this->walletId);
+
+        $wallet->balance += $this->amount * 0.01;
+        $wallet->save();
+
+        Transaction::create([
+            'wallet_id' => $this->walletId,
+            'type' => 'rebate',
+            'amount' => $this->amount * 0.01,
+        ]);
+    }
+}
